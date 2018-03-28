678e874dfb84a72c09e0de9ecee1108a571ea120
Almas Akchabayev <almas.akchabayev@gmail.com>
2018-03-28 19:22:02 +0600

    feat: remove unnecessary dependencies

--- generator/index.js
+++ generator/index.js
@@ -3,21 +3,6 @@ module.exports = api => {
     scripts: {
       storybook: "vue-cli-service start-storybook",
       "build-storybook": "vue-cli-service start-storybook"
-    },
-    devDependencies: {
-      "@storybook/addon-actions": "^3.3.15",
-      "@storybook/addon-knobs": "^3.3.15",
-      "@storybook/addon-notes": "^3.3.15",
-      "@storybook/addons": "^3.3.15",
-      "@storybook/vue": "^3.3.15",
-      "@types/storybook__addon-actions": "^3.0.3",
-      "@types/storybook__addon-knobs": "^3.2.3",
-      "@types/storybook__addon-links": "^3.3.0",
-      "@types/storybook__addon-notes": "^3.3.1",
-      "@types/storybook__vue": "^3.3.0",
-      storybook: "^1.0.0",
-      "vue-storybook": "^0.2.0",
-      "webpack-merge": "^4.1.2"
     }
   });
 
