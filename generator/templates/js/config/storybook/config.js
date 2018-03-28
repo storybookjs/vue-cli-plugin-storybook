b090898e2decd23e8e79b178b786adf5b18e290d
Almas Akchabayev <almas.akchabayev@gmail.com>
2018-03-28 19:59:01 +0600

    fix(generator): fix path to src folder

--- generator/templates/js/config/storybook/config.js
+++ generator/templates/js/config/storybook/config.js
@@ -1,6 +1,6 @@
 import { configure } from "@storybook/vue";
 
-const req = require.context("../src/stories", true, /.stories.js$/);
+const req = require.context("../../src/stories", true, /.stories.js$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
