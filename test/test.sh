12b8f1465fe5dc5bc65f17cdd03cf812d3ad5bda
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2018-09-21 02:31:45 +0200

    Install in tmp folder to be safe

--- test/test.sh
+++ test/test.sh
@@ -6,6 +6,7 @@ npx vue create --preset test/preset.json tmp
 cd tmp
 npm i ../*.tgz
 npx vue invoke storybook --type init
+npm i
 npm i --save-dev storybook-chromatic
 echo "import 'storybook-chromatic'" > config/storybook/chromatic.js
 cat config/storybook/chromatic.js config/storybook/config.js > config.js
