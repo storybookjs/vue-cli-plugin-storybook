da32c114c31f25ed5515ae7641a937c9d5af1189
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2020-01-13 11:50:49 +0530

    Updated chromatic tests

--- test/test.sh
+++ test/test.sh
@@ -10,7 +10,4 @@ npm i ../*.tgz
 npx vue invoke storybook --type init --semver '>=4.1.0'
 npm i
 npm i --save-dev storybook-chromatic
-echo "import 'storybook-chromatic'" > config/storybook/chromatic.js
-cat config/storybook/chromatic.js config/storybook/config.js > config.js
-mv config.js config/storybook/config.js
 npx chromatic test --script-name='storybook:serve' --exit-zero-on-changes --no-interactive --debug
