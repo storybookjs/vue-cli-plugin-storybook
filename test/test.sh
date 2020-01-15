c3c616afe29e1536b486d692dfabd93fcfd18b63
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2020-01-16 00:35:16 +0530

    Fixes #84 by removing thread-loader for storybook:build

--- test/test.sh
+++ test/test.sh
@@ -7,7 +7,7 @@ npm pack .
 npx vue create --preset test/preset.json tmp
 cd tmp
 npm i ../*.tgz
-npx vue invoke storybook --type init --semver '>=4.1.0'
+npx vue invoke storybook --type init --semver '>=5.3.0' --csf --docs
 npm i
 npm i --save-dev storybook-chromatic
 npx chromatic test --script-name='storybook:serve' --exit-zero-on-changes --no-interactive --debug
