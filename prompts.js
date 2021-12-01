1342c597f0e6cf9b1a2583f4f33eac947b384c3a
Bodo Graumann <mail@bodograumann.de>
2021-12-01 11:45:22 +0100

    Update minimal versions
    Webpack5 is only supported since storybook 6.2 and we should use
    storybook 6.4 if possible.

--- prompts.js
+++ prompts.js
@@ -17,15 +17,15 @@ module.exports = [
     when: (answers) => answers.type === 'init',
     name: 'semver',
     type: 'input',
-    default: '^6.0.26',
+    default: '^6.4.3',
     message: `What storybook version do you want? ${chalk.yellow('(Please specify semver range)')}`,
     validate: (input) => {
       if (input === '' || !semver.validRange(input)) {
         return 'Given semver range is not valid.';
       }
 
-      if (!semver.prerelease(input) && !semver.intersects(input, '>=6.0.0')) {
-        return 'Minimum supported storybook version is 6.0.0';
+      if (!semver.prerelease(input) && !semver.intersects(input, '>=6.2.0')) {
+        return 'Minimum supported storybook version is 6.2.0';
       }
 
       return true;
